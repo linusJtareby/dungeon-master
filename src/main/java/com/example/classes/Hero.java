293739c84eda336f4d1bbfc1d5cbc4621236499a
“linusJtareby” <linus.tareby@se.experis.com>
2023-08-03 13:39:22 +0200

    newMethod(equipArmor, equipWeapon)

--- src/main/java/com/example/classes/Hero.java
+++ src/main/java/com/example/classes/Hero.java
@@ -6,6 +6,12 @@ import java.util.HashMap;
 import com.example.classes.Item.ArmorTypes;
 import com.example.classes.Item.Slots;
 import com.example.classes.Item.WeaponType;
+import com.example.classes.heroClasses.Archer;
+import com.example.classes.heroClasses.Barbarian;
+import com.example.classes.heroClasses.Swashbuckler;
+import com.example.classes.heroClasses.Wizard;
+import com.example.classes.itemClasses.Armor;
+import com.example.classes.itemClasses.Weapon;
 
 public abstract class Hero {
     private String name;
@@ -30,11 +36,57 @@ public abstract class Hero {
 
     // Methods
     public void levelUp() {
+        this.level++;
 
+        if (this instanceof Wizard) {
+            this.heroAttribute.strength++;
+            this.heroAttribute.dexterity++;
+            this.heroAttribute.intelligence += 5;
+        }
+        if (this instanceof Archer) {
+            this.heroAttribute.strength++;
+            this.heroAttribute.dexterity += 7;
+            this.heroAttribute.intelligence++;
+        }
+        if (this instanceof Swashbuckler) {
+            this.heroAttribute.strength++;
+            this.heroAttribute.dexterity += 4;
+            this.heroAttribute.intelligence++;
+        }
+        if (this instanceof Barbarian) {
+            this.heroAttribute.strength += 3;
+            this.heroAttribute.dexterity += 2;
+            this.heroAttribute.intelligence++;
+        }
     }
 
-    public void equipWeapon() {
+    public void equipArmor(Slots slot, Armor armorToEquip) {
+        if (slot == Slots.Weapon) {
+            System.out.println("You can't equip armor to your weapon-slot");
+        } else {
+            if (armorToEquip.requiredLevel <= this.level) {
+                if (this.validArmorTypes.contains(armorToEquip.armorType)) {
+                    this.equipment.put(slot, armorToEquip);
 
+                } else {
+                    System.out.println("This armor is not valid for your class");
+                }
+            } else {
+                System.out.println("You need to be level " + armorToEquip.requiredLevel + " to equip this item");
+            }
+        }
+    }
+
+    public void equipWeapon(Weapon weaponToEquip) {
+        if (weaponToEquip.requiredLevel <= this.level) {
+            if (this.validWeaponTypes.contains(weaponToEquip.weaponType)) {
+                this.equipment.put(Slots.Weapon, weaponToEquip);
+            } else {
+                System.out.println("This weapon is not valid for your class");
+            }
+        } else {
+            System.out.println("You need to be level " + weaponToEquip.requiredLevel + " to equip this item");
+        }
     }
 
     public int damage() {
